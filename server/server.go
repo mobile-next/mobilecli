142d52cb01380a5b7e8b74b6ab699ca7360b5faf
Gil Megidish <gil@megidish.net>
2025-11-26 03:59:29 -0800

    fix: cache preflight cors check for a day (#114)

--- server/server.go
+++ server/server.go
@@ -79,6 +79,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Max-Age", "86400")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
