61e4642ba4f6e8e3bf500ec225718695f8106dba
Gil Megidish <gil@megidish.net>
2026-01-18 19:28:43 +0100

    fix: reduce logs on port already in use (#152)

--- utils/port-manager.go
+++ utils/port-manager.go
@@ -3,13 +3,17 @@ package utils
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 func IsPortAvailable(host string, port int) bool {
-	Verbose("Checking if port %d is available on %s", port, host)
+	// Verbose("Checking if port %d is available on %s", port, host)
 	listener, err := net.ListenTCP("tcp4", &net.TCPAddr{IP: net.ParseIP(host), Port: port})
 	if err != nil {
-		Verbose("error: %v", err)
+		// only log unexpected errors, not "address already in use"
+		if !strings.Contains(err.Error(), "address already in use") {
+			Verbose("error: %v", err)
+		}
 		return false
 	}
 
